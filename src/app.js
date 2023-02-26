e15eec249dee31fcea26a8a9a0beba0360c479ab
Martin Lopez <martin.lopez3@bbva.com>
2023-02-26 17:32:51 -0300

    feat(*): add send html files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('path') // normaliza rutas (// \\) windows, ios, linux
+
+
 app.use(require('./routes/index.routes'))
+
+
+//STATIC FILES
+app.use(express.static(path.join(__dirname, '../public')))
+app.use((req, res)=> {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 app.listen(3000, ()=>{
     console.log('Servidor conectado')
 })
